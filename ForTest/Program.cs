64a9b623f16e6b969f0c2b175a4afa85c9f73e02
axelssonmt <mattias.axelsson@cgi.com>
2017-07-13 12:39:29 +0200

    commit all to b4

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,16 +8,28 @@ namespace ForTest
         {
             Console.WriteLine("Enter the Temperature in Celsius : ");
             int celsius = int.Parse(Console.ReadLine());
-            int farenvalue = faren(celsius);
+            int farenvalue = Faren(celsius);
             Console.WriteLine("Temperature in Fahrenheit is : " + farenvalue);
+            RunForest("Running running!");
             Console.ReadLine();
         }
 
-        public static int faren (int celsius)
+        public static int Faren (int celsius)
         {
             int faren;
             faren = (celsius * 9) / 5 + 32;
             return faren;
         }
+
+        static void RunForest(string run)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            
+            Console.WriteLine(run);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        
     }
 }
