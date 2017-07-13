6609cde03e0ecacf542566b2637d95bf747e8bb9
axelssonmt <mattias.axelsson@cgi.com>
2017-07-13 12:46:03 +0200

    all again to b4

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,26 +10,43 @@ namespace ForTest
             int celsius = int.Parse(Console.ReadLine());
             int farenvalue = Faren(celsius);
             Console.WriteLine("Temperature in Fahrenheit is : " + farenvalue);
-            RunForest("Running running!");
+            if (farenvalue >= 64)
+            {
+                RunForest("Running running!");
+            }
+            else
+            {
+                StayHomeForest("To cold, stay home");
+            }
             Console.ReadLine();
         }
 
-        public static int Faren (int celsius)
+        public static int Faren(int celsius)
         {
             int faren;
             faren = (celsius * 9) / 5 + 32;
             return faren;
         }
 
-        static void RunForest(string run)
+        static void RunForest(string text)
         {
             Console.BackgroundColor = ConsoleColor.Green;
             Console.ForegroundColor = ConsoleColor.Black;
-            
-            Console.WriteLine(run);
+
+            Console.WriteLine(text);
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
         }
-        
+
+        static void StayHomeForest(string text)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine(text);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
     }
 }
