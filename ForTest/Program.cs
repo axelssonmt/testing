e3527e612cf11c2b8b395e17ba2f4f66c1299377
axelssonmt <mattias.axelsson@cgi.com>
2017-07-13 09:56:28 +0200

    b2 second

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,7 @@ namespace ForTest
             Console.WriteLine("Hello World!");
             Console.WriteLine("in b1 release");
             Console.WriteLine("in b2 release");
+            Console.WriteLine("after commit in in b2 release files was identical so ill  make third one.");
         }
     }
 }
