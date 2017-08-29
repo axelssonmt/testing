1b8758b124fcd42c5bef63f92443c7fe31d13343
axelssonmt <mattias.axelsson@cgi.com>
2017-08-29 16:04:58 +0200

    bull

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,31 +10,41 @@ namespace ForTest
         {
             Console.WriteLine(StringHelper.convertFromCelsiusToFahrenheitPress1);
             Console.WriteLine(StringHelper.convertFromFahrenheitToCelsiusPress2);
-            int input = int.Parse(Console.ReadLine());
+            var line = Console.ReadLine();
+            int value;
 
-            if(input == 1)
+            if(int.TryParse(line, out value))
             {
-                Console.WriteLine(StringHelper.enterTheTemperatureInCelsius);
-                double celsiusValue = int.Parse(Console.ReadLine());
-                double fahrenheitValue = Converters.FahrenheitConverter(celsiusValue);
-                Console.WriteLine(StringHelper.temperatureInFahrenheitIs + fahrenheitValue);
-                Console.ReadLine();
+                if (value == 1)
+                {
+                    Console.WriteLine(StringHelper.enterTheTemperatureInCelsius);
+                    double celsiusValue = int.Parse(Console.ReadLine());
+                    double fahrenheitValue = Converters.FahrenheitConverter(celsiusValue);
+                    Console.WriteLine(StringHelper.temperatureInFahrenheitIs + fahrenheitValue);
+                    Console.ReadLine();
 
-            }
-            else if(input == 2)
-            {
-                Console.WriteLine(StringHelper.enterTheTemperatureInFahrenheit);
-                double fahrenheitValue = int.Parse(Console.ReadLine());
-                double celsiusValue = Converters.CelsiusConverter(fahrenheitValue);
-                Console.WriteLine(StringHelper.temperatureInCelsiusIs + celsiusValue);
-                Console.ReadLine();
+                }
+                else if (value == 2)
+                {
+                    Console.WriteLine(StringHelper.enterTheTemperatureInFahrenheit);
+                    double fahrenheitValue = int.Parse(Console.ReadLine());
+                    double celsiusValue = Converters.CelsiusConverter(fahrenheitValue);
+                    Console.WriteLine(StringHelper.temperatureInCelsiusIs + celsiusValue);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(StringHelper.NotAValidEntry);
+                    Console.ReadLine();
+                }
             }
             else
             {
                 Console.BackgroundColor = ConsoleColor.DarkRed;
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine(StringHelper.NotAValidEntry);
-                Console.ReadLine();
+                Console.WriteLine("You need to enter a number (1 and 2 to convert)");
             }
         }
     }
